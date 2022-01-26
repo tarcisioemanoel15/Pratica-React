cb16dd775bc0a615f0bb85e77ccdd48d36ae556e
Tarcisio <caninosbrancos10031@gmail.com>
2022-01-26 20:49:54 -0300

    Click no Bbotao

--- 09.js
+++ 09.js
@@ -3,11 +3,11 @@
 // Renderizando mais elementos na pagina
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1(props) {
+function MeuComponente1() {
   return (
     < div className="componentes-1">
       <MeuComponente2 >
-        <MeuComponente3 onClickIncrementar={props.onClickIncrementar}/>
+        <MeuComponente3 />
       </MeuComponente2>
     </div>
   )
@@ -26,14 +26,14 @@ function MeuComponente2(props) {
 }
 
 
-function MeuComponente3(props) {
+function MeuComponente3() {
   const [telefone, setTelefone] = React.useState('74999 999 999');
  setTimeout(function(){
   setTelefone('1234567890')
 },150)
   return (
     <div id="componentes-3">
-      <MeuComponente4 telefone={telefone} onClickIncrementar={props.onClickIncrementar}/>
+      <MeuComponente4 telefone={telefone}/>
     </div>
   )
 }
@@ -43,28 +43,29 @@ function MeuComponente3(props) {
   
 
 function MeuComponente4(props) {
-
   const [idade, setIdade] = React.useState(28);
-
   setTimeout(function() {
-    setIdade(30)
+    setIdade(100)
   }, 1000)
+
+  // function clickBotao(){
+  //  console.log('clicouu');
+  // }
 
   return (
     <div className="componentes-4">
-      <p>Meu componente 4  {idade} - {props.telefone}</p>
-      <button type="bbutton" onClick={props.onClickIncrementar}>Incrementar</button>
+      <p>Meu componente 4 {props.nome} - {idade} - {props.telefone}</p>
+      <button type="bbutton" onClick={() => {console.log('clicouu');}}>Incrementar</button>
     </div>
   )
-
 }
 
 
 
-function MeuComponente(props) {
+function MeuComponente() {
   return (
     <div id="componentes">
-      <MeuComponente1 onClickIncrementar={props.onClickIncrementar}/>
+      <MeuComponente1 />
     </div>
   )
 }
@@ -87,14 +88,9 @@ function MeuComponenteIrmao2(props){
 
 function AppComponente(){
   const [contador, incrementaContador ] = React.useState(0);
-
-  const clickIncrementa = function(){
-    incrementaContador(contador + 1);
-  }
-
   return(
-    <React.Fragment >
-    <MeuComponente onClickIncrementar={clickIncrementa}/>
+    <React.Fragment>
+    <MeuComponente />
     <MeuComponenteIrmao contador={contador}/>
     </React.Fragment>
   )
--- dist.js
+++ dist.js
@@ -2,10 +2,12 @@
 // Renderizando mais elementos na pagina
 const NomeContext = React.createContext('nome');
 
-function MeuComponente1() {
+function MeuComponente1(props) {
   return /*#__PURE__*/React.createElement("div", {
     className: "componentes-1"
-  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement(MeuComponente3, null)));
+  }, /*#__PURE__*/React.createElement(MeuComponente2, null, /*#__PURE__*/React.createElement(MeuComponente3, {
+    onClickIncrementar: props.onClickIncrementar
+  })));
 }
 
 function MeuComponente2(props) {
@@ -14,7 +16,7 @@ function MeuComponente2(props) {
   }, /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("headers", null, props.children), /*#__PURE__*/React.createElement("footer", null, " ")));
 }
 
-function MeuComponente3() {
+function MeuComponente3(props) {
   const [telefone, setTelefone] = React.useState('74999 999 999');
   setTimeout(function () {
     setTelefone('1234567890');
@@ -22,32 +24,30 @@ function MeuComponente3() {
   return /*#__PURE__*/React.createElement("div", {
     id: "componentes-3"
   }, /*#__PURE__*/React.createElement(MeuComponente4, {
-    telefone: telefone
+    telefone: telefone,
+    onClickIncrementar: props.onClickIncrementar
   }));
 }
 
 function MeuComponente4(props) {
   const [idade, setIdade] = React.useState(28);
   setTimeout(function () {
-    setIdade(100);
-  }, 1000); // function clickBotao(){
-  //  console.log('clicouu');
-  // }
-
+    setIdade(30);
+  }, 1000);
   return /*#__PURE__*/React.createElement("div", {
     className: "componentes-4"
-  }, /*#__PURE__*/React.createElement("p", null, "Meu componente 4 ", props.nome, " - ", idade, " - ", props.telefone), /*#__PURE__*/React.createElement("button", {
+  }, /*#__PURE__*/React.createElement("p", null, "Meu componente 4  ", idade, " - ", props.telefone), /*#__PURE__*/React.createElement("button", {
     type: "bbutton",
-    onClick: () => {
-      console.log('clicouu');
-    }
+    onClick: props.onClickIncrementar
   }, "Incrementar"));
 }
 
-function MeuComponente() {
+function MeuComponente(props) {
   return /*#__PURE__*/React.createElement("div", {
     id: "componentes"
-  }, /*#__PURE__*/React.createElement(MeuComponente1, null));
+  }, /*#__PURE__*/React.createElement(MeuComponente1, {
+    onClickIncrementar: props.onClickIncrementar
+  }));
 }
 
 function MeuComponenteIrmao(props) {
@@ -64,7 +64,14 @@ function MeuComponenteIrmao2(props) {
 
 function AppComponente() {
   const [contador, incrementaContador] = React.useState(0);
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MeuComponente, null), /*#__PURE__*/React.createElement(MeuComponenteIrmao, {
+
+  const clickIncrementa = function () {
+    incrementaContador(contador + 1);
+  };
+
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(MeuComponente, {
+    onClickIncrementar: clickIncrementa
+  }), /*#__PURE__*/React.createElement(MeuComponenteIrmao, {
     contador: contador
   }));
 }
